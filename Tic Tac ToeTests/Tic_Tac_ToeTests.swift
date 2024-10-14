95eb0a5e93c3f75494a63650a18c93cfb78f2574
Nolan McSweeney <nolanbm2009@icloud.com>
2024-10-14 10:05:28 -0500

    Initial Commit

---
+++ Tic Tac ToeTests/Tic_Tac_ToeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeTests.swift
+//  Tic Tac ToeTests
+//
+//  Created by Nolan Brian McSweeney on 10/14/24.
+//
+
+import Testing
+@testable import Tic_Tac_Toe
+
+struct Tic_Tac_ToeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
